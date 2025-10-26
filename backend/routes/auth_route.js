fb32073c35221fa2acb9d80a84cc012c761073b4
Symon <fritzsymon23@gmail.com>
2025-10-26 15:27:23 +0800

    Auth Routes and Middleware

--- backend/routes/auth_route.js
+++ backend/routes/auth_route.js
@@ -1,8 +1,22 @@
 const express = require("express");
-
 const passport = require("passport");
 const bcrypt = require("bcrypt");
 
 const router = express.Router();
 
 const User = require("../models/User");
+
+// Error Handler
+const ExpressError = require("../middleware/ExpressError");
+
+router.post("/signup", async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return next(new ExpressError("User already Exists", 400));
+    }
+  } catch (err) {
+    next(err);
+  }
+});
