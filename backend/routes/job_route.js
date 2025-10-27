e7fc2071d95aad8a2761d29cc59c5e241ed3c7b8
Symon <fritzsymon23@gmail.com>
2025-10-28 00:41:15 +0800

    Finished basic CRUD for Jobs

--- backend/routes/job_route.js
+++ backend/routes/job_route.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const Job = require("../models/Job");
 const User = require("../models/User");
 
-router.post("/data", async (req, res, next) => {
+router.get("/data", async (req, res, next) => {
   try {
     const jobList = await Job.find();
     res.json(jobList);
@@ -17,7 +17,7 @@ router.post("/data/create", async (req, res, next) => {
   try {
     const { owner, company, role, platform, status, priority, notes } = req.body;
     const jobData = new Job({
-      owner,
+      userId: owner,
       company,
       role,
       platform,
@@ -27,11 +27,55 @@ router.post("/data/create", async (req, res, next) => {
     });
 
     const savedJob = await jobData.save();
+    console.log("Data Saved");
 
     res.status(201).json({
       success: true,
       message: "Job entry created successfully",
       data: savedJob,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:id/data", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const findJob = await Job.findByIdAndUpdate(id, req.body, {
+      runValidators: true,
+      new: true,
+    });
+
+    if (!findJob) {
+      return res.status(404).json({ message: "Job not found" });
+    }
+
+    res.status(201).json({
+      success: true,
+      message: "Job entry updated successfully",
+      data: findJob,
+    });
+    console.log("Successfully Updated");
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id/data/delete", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const deleteJob = await Job.findByIdAndDelete(id);
+
+    if (!deleteJob) {
+      return res.status(404).json({ message: "Job not Found" });
+    }
+
+    console.log("Successfulled Deleted");
+    res.status(201).json({
+      success: true,
+      message: "Job entry deleted successfully",
+      data: deleteJob,
     });
   } catch (err) {
     next(err);
