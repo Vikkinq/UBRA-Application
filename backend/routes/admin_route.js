98cf731232eaa651305ad0aca2d5dae18f4e882a
Symon <fritzsymon23@gmail.com>
2025-11-24 22:39:00 +0800

    GET

--- backend/routes/admin_route.js
+++ backend/routes/admin_route.js
@@ -2,11 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 const User = require("../models/User");
+const Job = require("../models/Job");
 
 router.get("/users", async (req, res, next) => {
-  const findUsers = await User.find({});
+  try {
+    const findUsers = await User.find({}).sort({ createdAt: -1 });
 
-  res.json(findUsers);
+    const userDocuments = await Job.countDocuments();
+
+    res.json(findUsers);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
