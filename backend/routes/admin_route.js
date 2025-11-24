06b075482a65c754be533fa790c26a6f210f46dc
Symon <fritzsymon23@gmail.com>
2025-11-24 22:34:25 +0800

    Starting Admin Dashboard

--- backend/routes/admin_route.js
+++ backend/routes/admin_route.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/admin", async (req, res, next) => {
-  res.json("Tae");
+const User = require("../models/User");
+
+router.get("/users", async (req, res, next) => {
+  const findUsers = await User.find({});
+
+  res.json(findUsers);
 });
 
 module.exports = router;
